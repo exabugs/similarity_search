c5d0fd8172e59162e4bafd52071f7f0372c20b01
exabugs <exabugs@gmail.com>
2013-12-17 13:47:07 +0900

    modified sample_search.js.

--- sample_search.js
+++ sample_search.js
@@ -1,52 +1,35 @@
-// Sample
-
-// Utility
-var util = {
-        // innerproduct
-        innerproduct : function (v1, v2) {
-                var sum = 0;
-                for(var key in v1) {
-                        if (v2[key]) {
-                                sum += v1[key] * v2[key];
-                        }
-                }
-                return sum;
-        },
-        // norm
-        norm : function (v) {
-                var sum = 0;
-                for(var key in v) {
-                        sum += v[key] * v[key];
-                }
-                return Math.sqrt(sum);
-        }
-}
-
+// 検索機能 サンプル
 var tweets_search = function (keyword) {
-	db.tweets_tfidf.mapReduce(
+	var condition = util.to_hash(keyword);
+printjson(condition);
+	// 検索の実行
+	db[master_name].mapReduce(
 		function () {
-			var s = util.innerproduct(this.value.v, keyword) / this.value.l
+			var s = util.innerproduct(this.tf.v, condition) / this.tf.l
 			if (0 < s) emit(this._id, s);
 		},
 		function(key,values) {
 			return values[0];
 		},
-		{scope: {util: util, keyword: keyword}, out: 'tweets_search_result'}
+		{scope: {util: util, condition: condition}, out: 'tweets_search_result'}
 	);
+	// 検索結果の表示
 	db.tweets_search_result.find().sort({value:-1}).limit(5).forEach(function (r) {
-		var result = db.tweets.findOne({_id:r._id},{content:1});
+		var result = db[master_name].findOne({_id:r._id},{content:1});
 		if (result) {
+			// 検索結果に類似したtweetを見つける
 			similar = [];
 			db.tweets_similarity.find({a:result._id},{b:1, score:1}).sort({score:-1}).limit(3).forEach(function (sr) {
 				var similar_result = db.tweets.findOne({_id:sr.b},{content:1});
 				if (similar_result)
 					similar.push({content: similar_result.content, score: sr.score});
 			});
+			// 検索結果の表示
 			printjson({content: result.content, score: r.value, similar: similar});
 		}
 	});
 }
 
-tweets_search({"job":1, "today":1});
-
-
+tweets_search([{k:"会社",w:1}]);
+tweets_search([{k:"お昼",w:1}]);
+tweets_search([{k:"お正月",w:1}]);
